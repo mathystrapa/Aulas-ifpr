a3ad46a8520828e8b525b9264ea08e3078a553a4
mathystrapa <mathystrapa@gmail.com>
2023-08-30 20:40:43 -0300

    Aula 2 - Importando pacotes

---
+++ POO/Aula 2 - Importando pacotes/Classes/classe_string.py
@@ -0,0 +1,12 @@
+class String:
+
+    def __init__(self) -> None:
+        pass
+
+    def caixa_alta(string):
+        upper_string = ''
+        for caractere in string:
+            if caractere == 'a':
+                upper_string += 'A'
+            elif caractere == 'b':
+                upper_string += 'B'
